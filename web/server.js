c3aae401f9bccf9e002d48506f6f3daaf6442588
Cristian Alberto Márquez <Cristian Alberto Mrquez>
2016-08-09 23:54:12 -0500

    new update

---
+++ web/server.js
@@ -0,0 +1,28 @@
+/**
+ * Created by warrdnez on 28/07/16.
+ */
+var connect         = require('connect');
+var http            = require('http');
+var directory       = './app/dist/';
+var serveStatic     = require('serve-static');
+var express         = require("express");
+var app             = express();
+var morgan          = require("morgan");
+var port            = process.env.PORT || 8080;
+var bodyParser      = require("body-parser");
+var methodOverride  = require("method-override");
+
+//para permitir parsear jsons
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//implementar y personalizar metodos http
+app.use(methodOverride());
+app.use(morgan("dev"));
+
+app = connect();
+
+app.use(serveStatic(directory));
+app.listen(3000);
+
+console.log('Listening on port 3000.');

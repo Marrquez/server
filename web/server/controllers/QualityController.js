f342257b3b90c060b20a64fc77e9975693dd3033
DESKTOP-NLPPM1R\usuario <warrdnez@hotmail.com>
2016-11-19 08:24:37 -0500

    updating the server

--- web/server/controllers/QualityController.js
+++ web/server/controllers/QualityController.js
@@ -4,8 +4,8 @@
  * @param res
  */
 exports.quality = function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+    res.header("Access-Control-Allow-Origin", "http://gualdo.s3-website-us-west-2.amazonaws.com/");
+    res.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
     var form = JSON.parse(req.body.tmpl);
